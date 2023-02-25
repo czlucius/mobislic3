da9c0e88d109c15f754f5a910e8c0a96dfad4404
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-02-25 16:18:20 +0800

    Update files

--- app/src/main/java/dev/czlucius/mobislic3/Slic3rFragment.kt
+++ app/src/main/java/dev/czlucius/mobislic3/Slic3rFragment.kt
@@ -1,10 +1,14 @@
 package dev.czlucius.mobislic3
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import dev.czlucius.mobislic3.databinding.FragmentSlic3Binding
 import org.the3deer.android_3d_model_engine.collision.CollisionController
 import org.the3deer.android_3d_model_engine.controller.TouchController
@@ -18,10 +22,12 @@ class Slic3rFragment : Fragment() {
 
     private var _binding: FragmentSlic3Binding? = null
 
+    private val args: Slic3rFragmentArgs by navArgs()
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private val glView: ModelSurfaceView? = null
+    private var glView: ModelSurfaceView? = null
     private val touchController: TouchController? = null
     private val scene: SceneLoader? = null
     private val collisionController: CollisionController? = null
@@ -31,7 +37,6 @@ class Slic3rFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
 
 
         _binding = FragmentSlic3Binding.inflate(inflater, container, false)
@@ -46,6 +51,21 @@ class Slic3rFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Snackbar.make(view, "the arg is ${args.fileUri}", Snackbar.LENGTH_LONG).show()
+
+        try {
+            Log.i("ModelActivity", "Loading GLSurfaceView...")
+//             glView = ModelSurfaceView(activity, backgroundColor, scene)
+//            glView!!.addListener(this)
+//            setContentView(glView)
+//            scene.setView(glView);
+        } catch (e: Exception) {
+            Log.e("ModelActivity", e.message, e)
+            Toast.makeText(
+                context, "Error loading OpenGL view:${e.message}", Toast.LENGTH_LONG).show()
+
+        }
 
     }
 
--- app/src/main/java/dev/czlucius/mobislic3/UploadFragment.kt
+++ app/src/main/java/dev/czlucius/mobislic3/UploadFragment.kt
@@ -1,24 +1,85 @@
 package dev.czlucius.mobislic3
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import dev.czlucius.mobislic3.databinding.FragmentUploadBinding
 
 
 class UploadFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentUploadBinding? = null
+    val binding: FragmentUploadBinding
+        get() = _binding!!
+
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Uri? = result.data?.data
+            if (data == null) {
+                view?.let { Snackbar.make(it, "Invalid file", Snackbar.LENGTH_SHORT).show() }
+                return@registerForActivityResult
+            }
+            // Since a Uri is a Parcelable, we can pass this to the Slicer fragment via SafeArgs.
+            val directions = UploadFragmentDirections.passFile(data)
+
+            findNavController().navigate(directions)
+
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentUploadBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false)
+        return _binding!!.root
+    }
+
+
+    fun openFile(pickerInitialUri: Uri?) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+
+
+        resultLauncher.launch(intent)
+
+    }
+
+
+
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.uploadButton.setOnClickListener {
+            // Upload the files here via SAF.
+            openFile(null)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
