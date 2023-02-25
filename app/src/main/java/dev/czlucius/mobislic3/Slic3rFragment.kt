c495ac810b06ad68b409f8cb207516dc7602fd92
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-02-25 12:32:23 +0800

    Update UI files

--- app/src/main/java/dev/czlucius/mobislic3/Slic3rFragment.kt
+++ app/src/main/java/dev/czlucius/mobislic3/Slic3rFragment.kt
@@ -1,30 +1,40 @@
 package dev.czlucius.mobislic3
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import dev.czlucius.mobislic3.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import dev.czlucius.mobislic3.databinding.FragmentSlic3Binding
+import org.the3deer.android_3d_model_engine.collision.CollisionController
+import org.the3deer.android_3d_model_engine.controller.TouchController
+import org.the3deer.android_3d_model_engine.services.SceneLoader
+import org.the3deer.android_3d_model_engine.view.ModelSurfaceView
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class Slic3rFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentSlic3Binding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val glView: ModelSurfaceView? = null
+    private val touchController: TouchController? = null
+    private val scene: SceneLoader? = null
+    private val collisionController: CollisionController? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+
+        _binding = FragmentSlic3Binding.inflate(inflater, container, false)
         return binding.root
 
     }
